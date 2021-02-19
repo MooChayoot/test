65142947c80ee95a1f1195411ab68cad46de5361
MooChayoot <moo.chayoot@gmail.com>
2021-02-19 15:41:12 +0700

    test01

---
+++ jq.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    a();
+});
+
+function a(){
+    let a = $(".a").text(
+        
+    )
+    a.click(()=>{
+        console.log("s");
+    })
+    console.log(a);
+}
